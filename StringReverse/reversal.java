d046a691637b0c3538f27f5df552ff544d0cafb2
Nandhiny-Duraikannu <nduraika@andrew.cmu.edu>
2017-12-19 18:19:00 -0800

    String Reversal

---
+++ StringReverse/reversal.java
@@ -0,0 +1,19 @@
+
+
+public class reversal {
+
+    public  reversal(String str) {
+
+        int len = str.length();
+        StringBuilder dest = new StringBuilder();
+
+        for (int i = (len - 1); i >= 0; i--)
+
+        {
+            dest.append(str.charAt(i));
+        }
+        System.out.println(dest.toString());
+    }
+
+
+}
